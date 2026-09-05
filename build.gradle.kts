727520e3b615d9e39dc0370cd601de5a581a618c
Ritwik Jamuar <13419671+codewithjams@users.noreply.github.com>
2026-09-05 23:55:48 +0530

    chore(gradle): scaffold multi-module Gradle project (#8)

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+	`java-library`
+}
+
+allprojects {
+	group = "io.url-shortener"
+	version = "0.1.0"
+
+	repositories {
+		mavenCentral()
+	}
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "url-shortener-shared-libs"
+
+include("links-contract")
+include("event-contracts")
+include("service-common")
