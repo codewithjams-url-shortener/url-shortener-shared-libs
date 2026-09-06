3c53010f3cd9eb52ace335c5263b15b06d8463a8
Ritwik Jamuar <13419671+codewithjams@users.noreply.github.com>
2026-09-06 19:56:52 +0530

    feat(gradle): publish all modules to GitHub Packages (#14)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
+
 plugins {
 	`java-library`
 }
@@ -14,10 +17,34 @@ allprojects {
 subprojects {
 
 	apply(plugin = "java-library")
+	apply(plugin = "maven-publish")
 
 	java {
 		toolchain {
 			languageVersion = JavaLanguageVersion.of(21)
+		}
+	}
+
+	configure<PublishingExtension> {
+		publications {
+			create<MavenPublication>("maven") {
+				from(components["java"])
+			}
+		}
+		repositories {
+
+			maven {
+
+				name = "GitHubPackages"
+				url = uri("https://maven.pkg.github.com/codewithjams-url-shortener/url-shortener-shared-libs")
+
+				credentials {
+					username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("gpr.user") as String?
+					password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.token") as String?
+				}
+
+			}
+
 		}
 	}
 
